17b89ac481331b67af307548fc90b32750f4e845
Skilled5041 <skilled5041@gmail.com>
2023-10-18 15:09:39 -0400

    Added tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import "../app.css";
+</script>
+
+<nav>
+	<h1>test</h1>
+</nav>
+
+<slot />
---
+++ src/routes/shortener/+page.svelte
@@ -0,0 +1,9 @@
+<form>
+	<div class="flex flex-col">
+		<label for="redirect-to">Redirect To</label>
+		<input type="text" name="redirect-to" id="redirect-to" placeholder="https://example.com" />
+		<label for="link-name">Link Name</label>
+		<input type="text" name="link-name" id="link-name" placeholder="Link Name" />
+		<button type="submit">Create Link</button>
+	</div>
+</form>
