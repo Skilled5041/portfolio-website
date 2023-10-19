7e3573b7113b6c88dc125f0d981dc5156d8a9d9e
Skilled5041 <skilled5041@gmail.com>
2023-10-19 17:29:29 -0400

    Improved ip page

--- src/routes/ip/+page.svelte
+++ src/routes/ip/+page.svelte
@@ -2,4 +2,4 @@
     export let data;
 </script>
 
-<h1>{data.ipAddress}</h1>
+<h1 class="min-h-screen flex items-center justify-center text-3xl uppercase">Your public ip address is {data.ipAddress}</h1>
